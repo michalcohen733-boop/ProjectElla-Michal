f5b8867866acb89dee126e0937c4b9e35a255a29
ellabentovim <Ella@bentovim.com>
2025-09-29 15:33:49 +0300

    add: main.py file

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+
+class VirusScan:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.base_url = "https://www.virustotal.com/api/v3/files/"
+
+    def get_virustotal_score(self, hash_value):
+        url = f"{self.base_url}{hash_value}"
+        headers = {
+            "accept": "application/json",
+            "x-apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        return None
+
+
+if __name__ == "__main__":
+    API_KEY = "4da69d055a6ed3dc5a364df98ec8e6ec92fb935bda1b63c852a99b5ee2da4b8f"
+    scanner = VirusScan(API_KEY)
+    file_hash = "420e57b017066b44e05ea1577f6e2e12"
+    stats = scanner.get_virustotal_score(file_hash)
+    print(stats)
